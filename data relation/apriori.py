32710250a9afa1fe2b31076038d45a2c7ef0ac88
Mehedee <razin.meha@gamil.com>
2020-05-29 11:47:07 +0900

    apriori end

--- data relation/apriori.py
+++ data relation/apriori.py
@@ -20,9 +20,17 @@ for i in range(0,7501):
 
 from apyori import apriori
 
-rules = apriori(transactions ,min_support=0.003 ,min_ confidence=0.2 ,min_lift=3,min_length=2)
+rules = apriori(transactions ,min_support=0.003 ,min_confidence=0.2 ,min_lift=3,min_length=2)
 
 
+results = list(rules)
+againlist= []
+
+i = 0
+for result in results:
+    print(result)
+    break
+    #againlist.append(str(list(*result)))
 
 
 
