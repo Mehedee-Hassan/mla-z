3352df20fa1c1c2444073007eb75529371d241f8
Mehedee <razin.meha@gamil.com>
2020-06-03 17:45:17 +0900

    kernel pca

--- PCA/kernel_PCA.py
+++ PCA/kernel_PCA.py
@@ -11,14 +11,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-path = "C:\\Users\\mehedee\\Documents\\Python Scripts\\tutorial\\Artificial_Neural_Networks\\ML_DS\\PCA\\"
+path = "C:\\Users\\mehedee\\Documents\\Python Scripts\\tutorial\\Artificial_Neural_Networks\\ML_DS\\regression\\"
 # Importing the dataset
 dataset = pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, [2,3]].values
 y = dataset.iloc[:, 4].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 # Feature Scaling
@@ -27,10 +27,18 @@ sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 
+# appling kernel pca
+from sklearn.decomposition import KernelPCA
+
+kpca = KernelPCA(n_components = 2, kernel = 'rbf') # number of principle components
+# we know we want to get two principle component here
+X_train = kpca.fit_transform(X_train)
+X_test = kpca.transform(X_test)
+
 
 #fitting logistic regression to training set
 from sklearn.linear_model import LogisticRegression
-classifier=LogisticRegression(random_state=0)
+classifierr=LogisticRegression(random_state=0)
 classifier.fit(X_train,y_train)
 
 
